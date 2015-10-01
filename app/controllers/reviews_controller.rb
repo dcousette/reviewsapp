4b734a64b93883f2bab874f9ad99979fd88bc72b
DeShawn <dcousette@gmail.com>
2015-10-01 05:12:50 +0000

    Reviews features

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,21 @@
 class ReviewsController < ApplicationController
-  def new 
-    @review = Review.new   
+  def new
+    @review = Review.new
   end
   
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.build(review_params)
-    @review.user_id = 1  #this needs to be changed to current_user.id
+    @review = @restaurant.reviews.build(review_params) 
+    @review.user_id = current_user.id
     
-    if @review.save 
+    # if @restaurant.reviews.reload.select {|review| review[:user_id] == current_user.id }
+    #   #does the restaurant have a review by the current user?
+      
+      
+    #   flash[:danger] = "Yo%li= link_to "Login", login_pathu can only write one review"
+    #   redirect_to restaurant_path(@restaurant)
+    
+    if @review.save
       flash[:success] = "Your review has been saved."
       redirect_to restaurant_path(@restaurant)
     else 
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,41 +2,47 @@ require 'rails_helper'
 
 describe ReviewsController do
   describe "POST create" do 
+    let(:user) { Fabricate(:user) }
+    let(:eats) { Fabricate(:restaurant) }
+    before { session[:user_id] = user.id }
+    
     context "with valid information" do
       it "sets up a restaurant object" do
-        eats = Fabricate(:restaurant)
-        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review)
+        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review, user: user)
         expect(assigns(:restaurant)).to eq(eats)
       end
       
       it "adds a review to the database" do 
-        eats = Fabricate(:restaurant)
-        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review)
+        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review, user: user)
         expect(Review.all.count).to eq(1)
       end 
       
-      it "flashes a success message" do 
-        eats = Fabricate(:restaurant)
-        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review)
+      it "flashes a success message" do
+        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review, user: user)
         expect(flash[:success]).to eq("Your review has been saved.")
       end
       
-      it "redirects to the restaurant page" do 
-        eats = Fabricate(:restaurant)
-        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review)
+      it "redirects to the restaurant page" do
+        post :create, restaurant_id: eats.id, review: Fabricate.attributes_for(:review, user: user)
         expect(response).to redirect_to restaurant_path(eats) 
+      end
+      
+      it "does not add a review for a restaurant if the user has already created one" do 
+        me = Fabricate(:user, id: 1 )
+        yums = Fabricate(:restaurant, id: 1, name: "Yum's")
+        review_too = Fabricate(:review, user: me, restaurant: yums)
+        post :create, restaurant_id: yums.id, review: { rating: 4, content: "Not bad!", user_id: 1 }
+        expect(Review.all.count).to eq(1)
       end
     end
     
     context "with invalid information" do  
       it "does not add a review to the database" do 
-        eats = Fabricate(:restaurant)
         post :create, restaurant_id: eats.id, review: { rating: 1 }
         expect(Review.all.count).to eq(0)
       end
       
       it "redirects to the restaurant page" do 
-        eats = Fabricate(:restaurant)
         post :create, restaurant_id: eats.id, review: { rating: 1 }
         expect(response).to redirect_to restaurant_path(eats)
       end

fd2c0ab8f8cfa0512f476005a65766689befcaad
DeShawn <dcousette@gmail.com>
2015-10-06 02:45:40 +0000

    Fix flash message display. Add new buttons for new restaurant & category.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id 
       redirect_to restaurants_path
     else 
+      flash[:danger] = "Incorrect Username or Password!"
       render :new 
     end
   end

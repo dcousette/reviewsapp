108e34a0b3e7452f930d786ba15921ac023e9cc5
DeShawn <dcousette@gmail.com>
2015-10-12 05:11:37 +0000

    DB seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,12 @@ Restaurant.create(name: "Ruth's Chris Steakhouse", description: "Ruth's Chris St
                   
 Restaurant.create(name: "Morton's The Steakhouse", description: "Morton’s The Steakhouse is a chain of more than 70 steak restaurants with 
                   locations in the United States and in cities in the USA and abroad. It is a wholly owned subsidiary of Landry's, Inc. 
-                  founded in Chicago in 1978.", image_url: "mortons2", category_id: 2 )
+                  founded in Chicago in 1978.", image_url: "mortons2.jpg", category_id: 2 )
                   
 Restaurant.create(name: "The Oceanaire Seafood Room", description: "Sleek and sophisticated, The Oceanaire provides the perfect setting to 
                   enjoy the freshest seafood flown in daily from around the world. Our knowledgeable staff will delight you with exceptional 
                   service — guiding you through the wide variety of items on our menu and helping you select the perfect wine to complement your meal. 
-                  You'll find out why The Oceanaire has been voted one of the best seafood restaurants in the country.", image_url: "oceanaire", category_id: 2)
+                  You'll find out why The Oceanaire has been voted one of the best seafood restaurants in the country.", image_url: "oceanaire.jpg", category_id: 2)
 
 Review.create(rating: 2, content: "This place rocks... Not!", 
               restaurant_id: 1, user_id: 1)

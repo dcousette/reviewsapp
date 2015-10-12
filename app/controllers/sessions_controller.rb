d051f1078d4951e03c1fc7b79f23820bfbac8aa3
DeShawn <dcousette@gmail.com>
2015-10-12 02:34:40 +0000

    Add success message for proper login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id 
+      flash[:success] = "Login successful!"
       redirect_to restaurants_path
     else 
       flash[:danger] = "Incorrect Username or Password!"
